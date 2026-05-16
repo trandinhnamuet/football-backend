22c32ce4897600f2ba594d3f05a41768153d3ad4
Trần Đình Nam <trandinhnamuet@gmail.com>
2026-05-16 22:36:30 +0700

    fix: remove AppService dep from AppController

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -1,16 +1,8 @@
 import { Controller, Get, UseGuards } from '@nestjs/common';
-import { AppService } from './app.service';
 import { AdminGuard } from './guards/admin.guard';
 
 @Controller()
 export class AppController {
-  constructor(private readonly appService: AppService) {}
-
-  @Get()
-  getHello(): string {
-    return this.appService.getHello();
-  }
-
   @Get('api/admin/verify')
   @UseGuards(AdminGuard)
   verifyAdmin() {
